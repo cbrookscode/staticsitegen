d1f00431fb221f0e7fa899ea09ceb3af5271696c
cbrookscode <colbybrooks@myyahoo.com>
2024-06-08 10:26:42 -0400

    updated up to 2.2

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+
+    node1 = TextNode("This is a text node", "bold", "https://www.boot.dev")
+
+    print(node1)
+
+# This ensures that the main() function runs when the script is executed
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,13 @@
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type 
+        self.url = url
+    
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return other.text == self.text and other.text_type == self.text_type and other.url == self.url
+    
+    def __repr__(self):
+        return f'TextNode({self.text!r}, {self.text_type!r}, {self.url!r})'
