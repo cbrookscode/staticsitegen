f18b3d5c84282363257caed4859711dc51630f90
cbrookscode <colbybrooks@myyahoo.com>
2024-10-01 13:48:02 -0400

    setup textnode class, and unittesting

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from textnode import TextNode
+
+def main():
+
+    test = TextNode("string", "bold", "http:site")
+    print(test.__repr__())
+
+if __name__=="__main__":
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,37 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+    def test_url_non(self):
+        node = TextNode("This is a text node", "bold", None)
+        node2 = TextNode("This is a text node", "bold", None)
+        self.assertEqual(node, node2)
+    def test_url_none_2(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold", None)
+        self.assertEqual(node, node2)
+    def test_different_textype(self):
+        node = TextNode("This is a text node", "italic", None)
+        node2 = TextNode("This is a text node", "bold", None)
+        self.assertNotEqual(node, node2)
+    def test_diff_text(self):
+        node = TextNode("This is a testy text node", "bold", None)
+        node2 = TextNode("This is a text node", "bold", None)
+        self.assertNotEqual(node, node2)
+    def test_empty_text(self):
+        node = TextNode("", "bold")
+        node2 = TextNode("", "bold")
+        self.assertEqual(node, node2)
+    def test_empty_type(self):
+        node = TextNode("This is a text node", "")
+        node2 = TextNode("This is a text node", "")
+        self.assertEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
