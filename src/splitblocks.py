3c3eafef4daf65794cadafe308a5e936088527b7
cbrookscode <colbybrooks@myyahoo.com>
2024-10-23 16:43:19 -0400

    finished block to block types function and unittesting for it

--- src/splitblocks.py
+++ src/splitblocks.py
@@ -8,3 +8,68 @@ def markdown_to_blocks(markdown):
         if block.strip():
             final.append(block.strip())
     return final
+
+def block_to_block_type(block_of_markdown):
+    if not block_of_markdown:
+        raise Exception("No markdown provided!")
+    if len(block_of_markdown) == 1:
+        return "paragraph"
+    split = block_of_markdown.split("\n")
+
+    # Check if heading type
+    if block_of_markdown[0] == "#":
+        for line in split:
+            count = 0
+            for i in range(1, 8):
+                if line[i] == "#":
+                    count += 1
+                elif count == 6:
+                    return "paragraph"
+                elif line[i] == " ":
+                    break
+                else:
+                    return "paragraph"
+        return "heading"
+            
+    # Check if code type
+    if block_of_markdown[0:3] == "```":
+        for line in split:
+            if line[0:3] != "```" or line[-3:] != "```":
+                return "paragraph"
+        return "code"
+    
+    # Check if quote type
+    if block_of_markdown[0] == ">":
+        for line in split:
+            if line[0] != ">":
+                return "paragraph"
+        return "quote"
+    
+    # Check if unordered list type
+    if block_of_markdown[0:2] == "* " or block_of_markdown[0:2] == "- ":
+        for line in split:
+            if line[0:2] != "* ":
+                if line[0:2] != "- ":
+                    return "paragraph"
+        return "unordered_list"
+    
+    # Check if ordered list type
+    if block_of_markdown[0].isdigit():
+        for line in split:
+            count = 0
+            for char in line:
+                if not char.isdigit():
+                    if char == ".":
+                        if line[count + 1] != " ":
+                            return "paragraph"
+                        else:
+                            break
+                    else:
+                        return "paragraph"
+                else:
+                    count += 1
+        return "ordered list"
+    
+    # Didn't find supported type so default to paragraph
+    else:
+        return "paragraph"
--- src/test_splitblocks.py
+++ src/test_splitblocks.py
@@ -1,4 +1,4 @@
-from splitblocks import markdown_to_blocks
+from splitblocks import markdown_to_blocks, block_to_block_type
 import unittest
 
 markdown_empty = ""
@@ -133,3 +133,73 @@ class TestMarkdownToBlocks(unittest.TestCase):
         result = markdown_to_blocks(markdown_one_block)
         expected = ['# This is a heading\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n## Heading 2\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n# Wow, just wow.']
         self.assertEqual(result, expected, f"Expected {expected} but got {result}")
+
+
+
+
+test = "###### "
+test2 = "###### ###How now brown cow!!####"
+test3 = "####### hiya"
+test5 = "#"
+test6 = "# "
+test7 = "# #Hello##"
+test8 = "``` Code Block ```"
+test9 = "```Code Block```"
+test10 = "``C`ode```"
+test11 = "`Code`"
+test12 = ">Quoteerino>>>>"
+test13 = ">This is the first list item in a list block\n>This is a list item\n>* This is another list item'"
+test14 = ">This is the first list item in a list block\nThis is a list item\n>* This is another list item'"
+test15 = "This is the first list item in a list block\n>This is a list item\n>* This is another list item'"
+test16 = "* This is the first list item in a list block\n>This is a list item\n* This is another list item'"
+test17 = "* This is the first list item in a list block\n* This is a list item\n* This is another list item'"
+test18 = "- This is the first list item in a list block\n- This is a list item\n- This is another list item'"
+test19 = "- This is the first list item in a list block\n* This is a list item\n- This is another list item'"
+test20 = "- This is the first list item in a list block\nThis is a list item\n- This is another list item'"
+test21 = "1. This is the first list item in a list block\n2. This is a list item\n3. This is another list item'"
+test22 = "This is the first list item in a list block\n2. This is a list item\n3. This is another list item'"
+test23 = "25555555555. This is the first list item in a list block\n2. This is a list item\n3000000000000. This is another list item'"
+test24 = "25555555555. This is the first list item in a list block\n20.This is a list item\n3000000000000. This is another list item'"
+test25 = "25555555555. This is the first list item in a list block\n20. This is a list item\n3000000000000. This is another list item'"
+test26 = "# Heading1\n# Heading2!"
+test27 = "# Heading1\n#Heading2!"
+test28 = "# Heading1\nwowowowoow"
+
+
+
+class TestBlockToBlockType(unittest.TestCase):
+
+    def test_all(self):
+        test_cases = [
+            (test, "heading"),
+            (test2, "heading"),
+            (test3, "paragraph"),
+            (test5, "paragraph"),
+            (test6, "heading"),
+            (test7, "heading"),
+            (test8, "code"),
+            (test9, "code"),
+            (test10, "paragraph"),
+            (test11, "paragraph"),
+            (test12, "quote"),
+            (test13, "quote"),
+            (test14, "paragraph"),
+            (test15, "paragraph"),
+            (test16, "paragraph"),
+            (test17, "unordered_list"),
+            (test18, "unordered_list"),
+            (test19, "unordered_list"),
+            (test20, "paragraph"),
+            (test21, "ordered list"),
+            (test22, "paragraph"),
+            (test23, "ordered list"),
+            (test24, "paragraph"),
+            (test25, "ordered list"),
+            (test26, "heading"),
+            (test27, "paragraph"),
+            (test28, "paragraph")
+        ]
+        for inp, expected in test_cases:
+            with self.subTest(inp=inp, expected=expected):
+                result = block_to_block_type(inp)
+                self.assertEqual(result, expected)
